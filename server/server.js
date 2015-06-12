91c0c257f15f1d5821caa850d85e3e7cd8717e34
pm-vaibhav-kakade <vaibhav.kakade@pubmatic.com>
2015-06-13 00:44:03 +0530

    Served basic index html file throught node server to client

--- server/server.js
+++ server/server.js
@@ -1,17 +1,79 @@
 var http = require("http");
-var server = http.createServer(function(request, response) {
-  response.writeHead(200, {"Content-Type": "text/html"});
-  response.write("<!DOCTYPE html>");
-  response.write("<html>");
-  response.write("<head>");
-  response.write("<title>Hello World Page</title>");
-  response.write("</head>");
-  response.write("<body>");
-  response.write("Hello World!");
-  response.write("</body>");
-  response.write("</html>");
-  response.end();
-});
+var server = http.createServer(connectionHandler);
+var PORT = 8989;
+var url = require("url");
+var path = require("path");
+var fs = require("fs");
 
-server.listen(80);
-console.log("Server is listening");
+function connectionHandler (request, response) {
+    var uri = url.parse(request.url).pathname,
+        filename = path.join(process.cwd(), uri);
+
+    fs.exists(filename, function(exists) {
+        if (!exists) {
+            response.writeHead(404, {
+                "Content-Type": "text/plain"
+            });
+            response.write("404 Not Found\n");
+            response.end();
+            return;
+        }
+
+        console.log(filename);
+        filename += '../client/index.html';
+
+        fs.readFile(filename, "binary", function(err, file) {
+          if(err) {
+            response.writeHead(500, {"Content-Type": "text/plain"});
+            response.write(err + "\n");
+            response.end();
+            return;
+          }
+
+          response.writeHead(200);
+          response.write(file, "binary");
+          response.end();
+        });
+    });
+
+}
+server.listen(PORT);
+console.log("Server is listening on port %s", PORT);
+
+/*
+var http = require("http"),
+    url = require("url"),
+    path = require("path"),
+    fs = require("fs"),
+    port = process.argv[2] || 8888;
+
+http.createServer(function(request, response) {
+
+  var uri = url.parse(request.url).pathname
+    , filename = path.join(process.cwd(), uri);
+
+  fs.exists(filename, function(exists) {
+    if(!exists) {
+      response.writeHead(404, {"Content-Type": "text/plain"});
+      response.write("404 Not Found\n");
+      response.end();
+      return;
+    }
+
+    if (fs.statSync(filename).isDirectory()) filename += '/index.html';
+
+    fs.readFile(filename, "binary", function(err, file) {
+      if(err) {
+        response.writeHead(500, {"Content-Type": "text/plain"});
+        response.write(err + "\n");
+        response.end();
+        return;
+      }
+
+      response.writeHead(200);
+      response.write(file, "binary");
+      response.end();
+    });
+  });
+}).listen(parseInt(port, 10));
+*/
