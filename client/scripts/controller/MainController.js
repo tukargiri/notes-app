fceae32447d48ca46c8f2965615d10aa69522882
pm-vaibhav-kakade <vaibhav.kakade@pubmatic.com>
2015-06-13 01:33:28 +0530

    Basic client setup

---
+++ client/scripts/controller/MainController.js
@@ -0,0 +1,7 @@
+"use strict";
+angular.module('notesApp')
+	.controller('MainCtrl', ['$scope',
+		function ($scope) {
+			$scope.requiredTrends = [];
+		}
+	]);
--- server/server.js
+++ server/server.js
@@ -1,3 +1,5 @@
+"use strict";
+
 var http = require("http");
 var server = http.createServer(connectionHandler);
 var PORT = 8989;
@@ -5,11 +7,11 @@ var url = require("url");
 var path = require("path");
 var fs = require("fs");
 
-function connectionHandler (request, response) {
+function connectionHandler(request, response) {
     var uri = url.parse(request.url).pathname,
         filename = path.join(process.cwd(), uri);
 
-    fs.exists(filename, function(exists) {
+    fs.exists(filename, function (exists) {
         if (!exists) {
             response.writeHead(404, {
                 "Content-Type": "text/plain"
@@ -19,61 +21,26 @@ function connectionHandler (request, response) {
             return;
         }
 
-        console.log(filename);
         filename += '../client/index.html';
 
-        fs.readFile(filename, "binary", function(err, file) {
-          if(err) {
-            response.writeHead(500, {"Content-Type": "text/plain"});
-            response.write(err + "\n");
-            response.end();
-            return;
-          }
+        fs.readFile(filename, "binary", function (err, file) {
+            if (err) {
+                response.writeHead(500, {
+                    "Content-Type": "text/plain"
+                });
+                response.write(err + "\n");
+                response.end();
+                return;
+            }
 
-          response.writeHead(200);
-          response.write(file, "binary");
-          response.end();
+            response.writeHead(200);
+            response.write(file, "binary");
+            response.end();
+
+            console.log("index.html is served!!!");
         });
     });
 
 }
 server.listen(PORT);
 console.log("Server is listening on port %s", PORT);
-
-/*
-var http = require("http"),
-    url = require("url"),
-    path = require("path"),
-    fs = require("fs"),
-    port = process.argv[2] || 8888;
-
-http.createServer(function(request, response) {
-
-  var uri = url.parse(request.url).pathname
-    , filename = path.join(process.cwd(), uri);
-
-  fs.exists(filename, function(exists) {
-    if(!exists) {
-      response.writeHead(404, {"Content-Type": "text/plain"});
-      response.write("404 Not Found\n");
-      response.end();
-      return;
-    }
-
-    if (fs.statSync(filename).isDirectory()) filename += '/index.html';
-
-    fs.readFile(filename, "binary", function(err, file) {
-      if(err) {
-        response.writeHead(500, {"Content-Type": "text/plain"});
-        response.write(err + "\n");
-        response.end();
-        return;
-      }
-
-      response.writeHead(200);
-      response.write(file, "binary");
-      response.end();
-    });
-  });
-}).listen(parseInt(port, 10));
-*/
