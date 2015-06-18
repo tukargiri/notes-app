722f1c7d4fd8fd73b6f387fa9280d039efdeec3c
pm-vaibhav-kakade <vaibhav.kakade@pubmatic.com>
2015-06-18 17:30:34 +0530

    Code clean up and support for error codes

--- server/mongoInterface.js
+++ server/mongoInterface.js
@@ -1,90 +1,139 @@
 "use strict";
 
+/*
+---------------------------
+	Mongo db commands
+---------------------------
+
+show dbs :- list of all collections
+db.collectionName.insert({a:10}) - adds the object in the collection
+db.collectionName.remove({}) - removes all rows the collection
+use [database]; db.dropDatabase(); - removes the db
+----------------------------
+
+// TODO :- find a way to insert following row, if counters collection does not exists
+db.counters.insert({ _id: "notesId", seq: 0 });
+db.counters.findAndModify({query: {_id: "notedId"}, update: {$inc: {seq: 1}}, new: true});
+
+*/
+
 var mongo = require('mongodb');
 var mongoClient = mongo.MongoClient;
-var ObjectID = require('mongodb').ObjectID;
-var COLLECTION_NAMES = ["counters", "notes"];
-
-// Connection URL
+var ObjectID = mongo.ObjectID;
 var url = 'mongodb://localhost:27017/diarydb';
+
 var DB;
-// Use connect method to connect to the Server
+var COLLECTION_NAMES = {
+	COUNTERS: "counters",
+	NOTES: "notes"
+};
+var RESPONSE = {
+	SUCCESS: "success",
+	SUCCESS_CODES: {
+		FIND: "Data searched successful",
+		INSERT: "Data inserted successfully",
+		DELETE: "Data deleted successfully",
+		UPDATE: "Data updated successfully"
+	},
+	ERROR: "error",
+	ERROR_CODES: {
+		501: "DB Error 501 :- cannot find data for given search criteria",
+		502: "DB Error 502 :- cannot insert data",
+		503: "DB Error 503 :- cannot delete data for given key",
+		504: "DB Error 504 :- cannot update given record"
+	}
+};
+
+var resposeObj = {
+	type: "",
+	message: "",
+	items: []
+};
+
+
 mongoClient.connect(url, function (err, db) {
 	console.log("Connected to mongo server");
 	DB = db;
 });
 
-// TODO :- find a way to insert following row, if counters collection does not exists
-// db.counters.insert({ _id: "notesId", seq: 0 });
-// db.counters.findAndModify({query: {_id: "notedId" }, update: {$inc: {seq: 1 } }, new: true });
+
 var mongoInterface = {
 	find: function (param, callback) {
-		var notesCollection = DB.collection(COLLECTION_NAMES[1]);
-		var searchParams = {};
+		var notesCollection = DB.collection(COLLECTION_NAMES.NOTES),
+			searchParams = {};
 
-		if (Object.keys(param).length) {
-			if (param._id) {
-				var reqId = new ObjectID(searchParams._id);
-				searchParams._id = reqId;
-			} else {
-				console.log("Please pass id");
-				// callback(null);
-			}
+		if (Object.keys(param).length && param._id) {
+			searchParams._id = new ObjectID(searchParams._id);
 		}
 
 		notesCollection.find(searchParams).toArray(function (err, data) {
 			if (err) {
-				// console.log("No title found");
-				callback(null);
+				resposeObj.type = RESPONSE.ERROR;
+				resposeObj.message = RESPONSE.ERROR_CODES[501];
+
 			} else {
-				console.log("collection found ", data);
-				callback(data);
+				resposeObj.type = RESPONSE.SUCCESS;
+				resposeObj.message = RESPONSE.SUCCESS_CODES.FIND;
+				resposeObj.items = data;
 			}
+			callback(resposeObj);
 		});
 	},
 	insert: function (data, callback) {
-		var notesCollection = DB.collection(COLLECTION_NAMES[1]);
+		var notesCollection = DB.collection(COLLECTION_NAMES.NOTES);
 
 		notesCollection.insert(data, function (err, result) {
 			if (err) {
-				console.log("Data cannot be saved");
-				callback(null);
+				resposeObj.type = RESPONSE.ERROR;
+				resposeObj.message = RESPONSE.ERROR_CODES[502];
+				resposeObj.items = [];
 			} else {
-				callback(result);
+				resposeObj.type = RESPONSE.SUCCESS;
+				resposeObj.message = RESPONSE.SUCCESS_CODES.INSERT;
+				resposeObj.items = result;
 			}
+			callback(resposeObj);
 		});
 	},
-	delete: function (data, callback) {
-		var notesCollection = DB.collection(COLLECTION_NAMES[1]);
-		var reqId = new ObjectID(data._id);
+	delete: function (id, callback) {
+		var notesCollection = DB.collection(COLLECTION_NAMES.NOTES);
 
 		notesCollection.remove({
-			"_id": reqId
+			"_id": new ObjectID(id)
 		}, function (err, result) {
 			if (err) {
-				callback(null);
+				resposeObj.type = RESPONSE.ERROR;
+				resposeObj.message = RESPONSE.ERROR_CODES[503];
+				resposeObj.items = [];
 			} else {
-				callback(result);
+				resposeObj.type = RESPONSE.SUCCESS;
+				resposeObj.message = RESPONSE.SUCCESS_CODES.DELETE;
+				resposeObj.items = result;
 			}
+			callback(resposeObj);
 		});
 	},
-	update: function (data, callback) {
-		var notesCollection = DB.collection(COLLECTION_NAMES[1]);
-		var reqId = new ObjectID(data._id);
+	update: function (newData, callback) {
+		var notesCollection = DB.collection(COLLECTION_NAMES.NOTES);
 
 		notesCollection.update({
-			"_id": reqId
+			"_id": new ObjectID(newData._id)
 		}, {
 			$set: {
-				title: data.title,
-				data: data.data
+				title: newData.title,
+				data: newData.data
 			}
 		}, function (err, result) {
 			if (err) {
-				callback(null);
+				resposeObj.type = RESPONSE.ERROR;
+				resposeObj.message = RESPONSE.ERROR_CODES[504];
+				resposeObj.items = [];
 			} else {
-				callback(result);
+				resposeObj.type = RESPONSE.SUCCESS;
+				resposeObj.message = RESPONSE.SUCCESS_CODES.UPDATE;
+				resposeObj.items = result;
 			}
+			callback(resposeObj);
 		});
 	}
 };
